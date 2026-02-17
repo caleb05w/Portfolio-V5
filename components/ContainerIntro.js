1acf748f2d49ad5ce5bfdda42dee78eb9143fda2
Caleb Wu <caleb05w@gmail.com>
2026-02-17 02:02:45 -0800

    testing

--- components/ContainerIntro.js
+++ components/ContainerIntro.js
@@ -1,37 +1,45 @@
-import React from 'react'
-import ContainerText from './containerText'
-import ContainerImagesShowcase from './containerImagesShowcase'
+import React from "react";
+import ContainerText from "./containerText";
+import ContainerImagesShowcase from "./containerImagesShowcase";
 
-function ContainerIntro({ Teammates, Name, Year, body, videoSrc, image2, image3 }) {
-    return (
-        <div className="flex flex-col gap-[5.125rem] case-x-gutter">
-            <div className="flex flex-col lg:gap-[2rem] md:gap-[2rem] gap-[2rem] md:flex-row xl:flex-row lg:flex-row justify-between">
-                <div className="flex flex-col gap-[1.5rem] md:max-w-[60%] xl:max-w-[60%] lg:max-w-[60%]">
-                    <h2>{Name}</h2>
+function ContainerIntro({
+  Teammates,
+  Name,
+  Year,
+  body,
+  videoSrc,
+  image2,
+  image3,
+}) {
+  return (
+    <div className="flex flex-col gap-[5.125rem] case-x-gutter">
+      <div className="flex flex-col lg:gap-[2rem] md:gap-[2rem] gap-[2rem] md:flex-row xl:flex-row lg:flex-row justify-between">
+        <div className="flex flex-col gap-[1.5rem] md:max-w-[60%] xl:max-w-[60%] lg:max-w-[60%]">
+          <h2>{Name}</h2>
 
-                    {body.map((item, key) => (
-                        <p key={key}> {item}</p>
-                    ))}
-                </div>
-                <div className='flex flex-col justify-between lg:gap-[0rem] md:gap-[0rem] gap-[1rem] md:w-[30%] lg:w-[30%] xl:w-[30%]'>
-                    <div className="flex flex-col gap-[1rem]">
-                        <h2> Team </h2>
-                        <div className="flex flex-col gap-[0]">
-                            {Teammates.map((item, key) => (
-                                <p key={key}>{item}</p>
-                            ))}
-                        </div>
-                    </div>
-                    <p>{Year}</p>
-                </div>
+          {body.map((item, key) => (
+            <p key={key}> {item}</p>
+          ))}
+        </div>
+        <div className="flex flex-col justify-between lg:gap-[0rem] md:gap-[0rem] gap-[1rem] md:w-[30%] lg:w-[30%] xl:w-[30%]">
+          <div className="flex flex-col gap-[1rem]">
+            <h2> Team </h2>
+            <div className="flex flex-col gap-[0]">
+              {Teammates.map((item, key) => (
+                <p key={key}>{item}</p>
+              ))}
             </div>
-            <ContainerImagesShowcase
+          </div>
+          <p>{Year}</p>
+        </div>
+      </div>
+      {/* <ContainerImagesShowcase
                 videoSrc={videoSrc}
                 image2={image2}
                 image3={image3}
-            />
-        </div>
-    )
+            /> */}
+    </div>
+  );
 }
 
-export default ContainerIntro
+export default ContainerIntro;
--- components/containerImagesShowcase.js
+++ components/containerImagesShowcase.js
@@ -1,60 +1,66 @@
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 function containerImagesShowcase({ videoSrc, image2, image3 }) {
-    // Use image2 for image1 if image1 is not provided (first can use same as second)
-    const secondImage = image2;
-    const thirdImage = image3;
+  // Use image2 for image1 if image1 is not provided (first can use same as second)
+  const secondImage = image2;
+  const thirdImage = image3;
 
-    return (
-        <div className="flex flex-col gap-[0.5rem] w-full">
-            {videoSrc &&
-                <div className='w-full h-full bg-[#F8F8F8] items-center justify-center flex flex-row p-[2rem]'>
-                    <video
-                        className="top-0 left-0 md:max-w-[25rem] xl:max-w-[25rem] lg:max-w-[25rem] aspect-square"
-                        src={videoSrc}
-                        autoPlay
-                        muted
-                        loop
-                        playsInline
-                        preload="metadata"
-                        style={{
-                            backgroundColor: "#F8F8F8",
-                            objectFit: "contain",
-                        }}
-                    />
-                </div>
-            }
-            {(secondImage || thirdImage) && (
-                <div className="flex flex-row gap-[0.5rem] w-full max-w-full">
-                    {secondImage && (
-                        <div className="flex-[3] min-w-0 h-auto">
-                            <Image
-                                src={secondImage.src}
-                                width={secondImage.width}
-                                height={secondImage.height}
-                                alt={secondImage.alt || ''}
-                                className={secondImage.className || 'object-cover w-full h-full'}
-                                loading="lazy"
-                            />
-                        </div>
-                    )}
-                    {thirdImage && (
-                        <div className={secondImage ? "flex-[7] min-w-0 h-auto" : "w-full h-auto"}>
-                            <Image
-                                src={thirdImage.src}
-                                width={thirdImage.width}
-                                height={thirdImage.height}
-                                alt={thirdImage.alt || ''}
-                                className={thirdImage.className || 'object-cover w-full h-auto'}
-                                loading="lazy"
-                            />
-                        </div>
-                    )}
-                </div>
-            )}
+  return (
+    <div className="flex flex-col gap-[0.5rem] w-full">
+      {videoSrc && (
+        <div className="w-full h-full bg-[#F8F8F8] items-center justify-center flex flex-row p-[2rem]">
+          <video
+            className="top-0 left-0 md:max-w-[25rem] xl:max-w-[25rem] lg:max-w-[25rem] aspect-square"
+            src={videoSrc}
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="metadata"
+            style={{
+              backgroundColor: "#F8F8F8",
+              objectFit: "contain",
+            }}
+          />
         </div>
-    )
+      )}
+      {(secondImage || thirdImage) && (
+        <div className="flex flex-row gap-[0.5rem] w-full max-w-full">
+          {secondImage && (
+            <div className="flex-[1] min-w-0 h-auto">
+              <Image
+                src={secondImage.src}
+                width={secondImage.width}
+                height={secondImage.height}
+                alt={secondImage.alt || ""}
+                className={
+                  secondImage.className || "object-cover w-full h-full"
+                }
+                loading="lazy"
+              />
+            </div>
+          )}
+          {thirdImage && (
+            <div
+              className={
+                secondImage ? "flex-[2] min-w-0 h-auto" : "w-full h-auto"
+              }
+            >
+              <Image
+                src={thirdImage.src}
+                width={thirdImage.width}
+                height={thirdImage.height}
+                alt={thirdImage.alt || ""}
+                className={thirdImage.className || "object-cover w-full h-auto"}
+                loading="lazy"
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default containerImagesShowcase
+export default containerImagesShowcase;
